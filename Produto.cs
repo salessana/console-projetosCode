d351d8fa44c4c1b296e613b3d6bd6f2afd5a50d9
ANA CAROLLYNA SALES DE SOUZA <53353942858@info.local>
2026-03-11 13:12:00 -0300

    voltando para o meu computador

--- Produto.cs
+++ Produto.cs
@@ -1,6 +1,6 @@
 class Produto
 {
-private static int contadorid= 1;
+    private static int contadorid = 1;
 
     public int Id { get; set; }
     public string? Nome { get; set; }
@@ -16,25 +16,44 @@ private static int contadorid= 1;
 
         string precoFormatado = produtoParaCadastrar.Preco.ToString("F2");
         Console.WriteLine($"Produto cadastrado com sucesso! Id: {produtoParaCadastrar.Id} | Nome: {produtoParaCadastrar.Nome} | Preço: {precoFormatado} ");
-        
-        
     }
 
-public void RemoverProduto(int Id)
+
+    public void RemoverProduto(int Id)
     {
         Produto? prod = produtos.Find(p => p.Id == Id);
         if (prod != null)
         {
             produtos.Remove(prod);
             Console.WriteLine($"Produto foi removido com sucesso! \n ");
-            
+
         }
         else
         {
-            Console.WriteLine($"produto nao");
-            
+            Console.WriteLine($"produto nao \n");
+
         }
     }
 
+
+    public void ListarProdutos()
+    {
+        console.WriteLine($"=== Lista de Produtos === ");
+        if (produto.Count == 0)
+        {
+            console.WriteLine($"Nenhum produto cadastrado. \n");
+            return;
+
+        }
+        foreach (var item in produtos)
+        {
+            console.WriteLine($"Id: {item.Id} | Nome: {item.Categoria} | Preço: {precoFormatado}");
+        }
+        
+    }
+
+public bool ListaVazia(){
+    return produtos.Count == 0; //retorna true ou false
+}
 
 }
